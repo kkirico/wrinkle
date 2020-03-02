3011ba838477548f67ec731673f10847a3a77fb1
Heonjun LEE <heonjun.lee@epita.fr>
2020-03-02 21:41:55 +0900

    id및 사소한 디자인 변경

--- app/src/main/java/com/flagtag/wrinkle/LoginActivity.java
+++ app/src/main/java/com/flagtag/wrinkle/LoginActivity.java
@@ -1,14 +1,96 @@
 package com.flagtag.wrinkle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class loginActivity extends AppCompatActivity {
+
+    private static final String TAG = "loginActivity";
+    private static final int RC_SIGN_IN = 123;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+    }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //UI
+    }
+
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.loginButton:
+                    startMainActivity();
+                    break;
+
+            }
+        }
+    };
+
+    private void signup() {
+
+        String email = ((EditText) findViewById(R.id.loginEmail)).getText().toString();
+        String password = ((EditText) findViewById(R.id.loginPassword)).getText().toString();
+
+
+        if (email.length()>0 && password.length()>0) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        startToast("로그인 성공");
+                        startMainActivity();
+                    } else {
+                        if (task.getException() != null) {
+                            startToast(task.getException().toString());
+                        }
+                    }
+                }
+            });
+        }
+        else{
+            startToast("이메일 또는 비밀번호가 입력되지 않았습니다.");
+        }
+    }
+    //토스트 메시지띄우는 함수
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void startMainActivity(){
+        Intent intent = new Intent( this,MainActivity.class);
+        intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
