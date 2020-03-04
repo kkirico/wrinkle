ed3323b9a3ff58f6b2c88f5ae9dfb1c3a49760bc
Heonjun LEE <heonjun.lee@epita.fr>
2020-03-04 19:29:01 +0900

    구글로그인 연동

---
+++ app/src/main/java/com/flagtag/wrinkle/googleLoginActivity.java
@@ -0,0 +1,131 @@
+package com.flagtag.wrinkle;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.internal.GoogleApiAvailabilityCache;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class googleLoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private SignInButton btn_google;
+    private Button btn_guestLogin;
+    private FirebaseAuth auth;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_SIGN_GOOGLE = 100;//구글 로그인 결과코드
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_login);
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,  this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions)
+                .build();
+
+
+        auth = FirebaseAuth.getInstance(); //파이어베이스 인증객체 초기화
+
+        btn_google = findViewById(R.id.googleLoginBtn );
+        btn_guestLogin = findViewById(R.id.guestLogin );
+        btn_google.setOnClickListener(new View.OnClickListener() {//구글로그인 버튼을 클릭했을때 시행되는 부분
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,REQ_SIGN_GOOGLE);
+            }
+        });
+        btn_guestLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(googleLoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, @Nullable Intent data){//구글 로그인 인증을 요청했을때 결과값을 돌려받는 곳
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == REQ_SIGN_GOOGLE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){//인증결과가 성공적이면
+                GoogleSignInAccount account = result.getSignInAccount();//어카운트라는 데이터는 구글 로그인 정보를 모두 담고있음 프사, 이메일주소 등
+                resultLogin(account);//로그인 결과값 출력 수행하라는 메소드
+
+            }
+        }
+    }
+
+    private void resultLogin(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){//login success
+                            Toast.makeText(googleLoginActivity.this, "로그인 성공",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            //intent.putExtra("nickname", account.getDisplayName());
+                            //intent.putExtra("photoUrl",String.valueOf(account.getPhotoUrl()));//특정 자료형을 스트링으로 변환할때 스트링.벨류오프
+
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText( googleLoginActivity.this, "로그인 실패",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+/*
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.guestLogin:
+                    guestLoginActivity();
+                    break;
+
+            }
+        }
+    };
+
+    private void guestLoginActivity(){
+        Intent intent = new Intent(googleLoginActivity.this,loginActivity.class);
+        startActivity(intent);
+    }
+*/
+}
