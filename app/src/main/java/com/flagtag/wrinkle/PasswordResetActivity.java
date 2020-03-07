95fe3d55a6a9cc2cadb66e5b2fde5ce3646e23cd
Heonjun LEE <heonjun.lee@epita.fr>
2020-03-07 21:44:04 +0900

    비밀번호 재설정 구현

---
+++ app/src/main/java/com/flagtag/wrinkle/PasswordResetActivity.java
@@ -0,0 +1,81 @@
+package com.flagtag.wrinkle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class PasswordResetActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.sendNewPassword).setOnClickListener(onClickListener);
+    }
+
+
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.sendNewPassword:
+                    send();
+                    break;
+
+            }
+        }
+    };
+
+    private void send() {
+
+        String email = ((EditText) findViewById(R.id.loginEmail)).getText().toString();
+
+        if (email.length()>0 ) {
+            mAuth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                startToast("이메일을 보냈습니다.");
+                            }
+                        }
+                    });
+
+
+        }
+        else{
+            startToast("이메일을 입력해주세요.");
+        }
+    }
+    //토스트 메시지띄우는 함수
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    private void startMainActivity(){
+        Intent intent = new Intent( this,MainActivity.class);
+        //intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
