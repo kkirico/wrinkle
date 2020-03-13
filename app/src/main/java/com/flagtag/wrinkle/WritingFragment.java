4e24310b13c914bd87f07569205415de31d6b413
j03y14 <j03y14@gmail.com>
2020-03-13 20:14:05 +0900

    이미지 추가 기능 만듦

--- app/src/main/java/com/flagtag/wrinkle/WritingFragment.java
+++ app/src/main/java/com/flagtag/wrinkle/WritingFragment.java
@@ -1,25 +1,56 @@
 package com.flagtag.wrinkle;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class WritingFragment extends Fragment {
 
     FirebaseFirestore db;
     Toolbar toolbar;
+    LinearLayout writing_content_container;
+    ArrayList<View> contentArray;
+    EditText writing1;
+    MainActivity activity;
 
+
+    private static final int PICK_FROM_ALBUM = 1;
     public WritingFragment() {
         // Required empty public constructor
     }
@@ -30,14 +61,45 @@ public class WritingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_writing, container, false);
+        activity = (MainActivity)getActivity();
 
         // Access a Cloud Firestore instance from your Activity
         db = FirebaseFirestore.getInstance();
 
+        writing_content_container = rootView.findViewById(R.id.content_container);
+        writing1 = rootView.findViewById(R.id.writing_content1);
+        contentArray = new ArrayList<View>();
+        contentArray.add(writing1);
+
         toolbar = rootView.findViewById(R.id.writing_fragment_toolbar);
-        //MainActivity mainActivity = (MainActivity) getActivity();
-        //mainActivity.setSupportActionBar(toolbar);
-        //ActionBar actionBar = mainActivity.getSupportActionBar();
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.image_button){
+
+                    //이미지 선택
+                    startToast("이미지 선택");
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, PICK_FROM_ALBUM);
+                }else if(item.getItemId() == R.id.vidoe_button){
+                    //동영상 선택
+                }else if(item.getItemId() == R.id.location_button){
+                    //위치 선택
+                }else if(item.getItemId() == R.id.quote_button){
+                    //인용 버튼
+                }else if(item.getItemId() == R.id.division_line_button){
+                    //구분선 버튼
+                }else if(item.getItemId() == R.id.more_button){
+                    //더보기 버튼
+                    //아직 더보기 버튼을 어떻게 사용할지는 정하지 않았음
+                }
+                return false;
+            }
+        });
+
+
 
 
 
@@ -59,4 +121,66 @@ public class WritingFragment extends Fragment {
 
     }
 
+    private void startToast(String msg){
+        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICK_FROM_ALBUM){
+            try {
+                // 선택한 이미지에서 비트맵 생성
+                InputStream in = activity.getContentResolver().openInputStream(data.getData());
+                Bitmap img = BitmapFactory.decodeStream(in);
+                in.close();
+                // 이미지뷰에 세팅
+                ImageView imageView = new ImageView(activity);
+                imageView.setImageBitmap(img);
+
+
+
+                writing_content_container.addView(imageView,0);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void checkSelfPermission() {
+        String temp = "";
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+        if (TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(activity, temp.trim().split(" "),1);
+        }else {
+            // 모두 허용 상태
+            startToast("권한을 모두 허용");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) { // 동의
+                    Log.d("MainActivity","권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+
+    }
 }
