f3dedd8f5cce85af4c70251e0054dc61e793c68b
j03y14 <j03y14@gmail.com>
2020-03-16 22:47:08 +0900

    이미지 select 하는 경우 custom view 수정중

---
+++ app/src/main/java/com/flagtag/wrinkle/WritingImageView.java
@@ -0,0 +1,56 @@
+package com.flagtag.wrinkle;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class WritingImageView extends ConstraintLayout {
+
+    ConstraintLayout background;
+    ImageView imageView;
+    EditText text;
+
+    private static final int[] mStates = { R.attr.state_notset, R.attr.state_on, R.attr.state_off };
+    private int mStateIndex = 0;
+    public WritingImageView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public WritingImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WritingImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    public void initView(){
+        String inflaterService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater layoutInflater = (LayoutInflater)getContext().getSystemService(inflaterService);
+        View view = layoutInflater.inflate(R.layout.writing_imageview,this,false);
+        addView(view);
+
+        background = findViewById(R.id.background);
+        imageView = findViewById(R.id.writing_image);
+        text = findViewById(R.id.writing_text);
+    }
+
+    public void setImageView(Bitmap bitmap){
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+        super.setOnClickListener(l);
+    }
+}
