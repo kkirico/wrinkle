fb20df690402ccfe6dff6df296fb419c646e5b0a
j03y14 <j03y14@gmail.com>
2020-05-18 22:06:52 +0900

    태그된 유저 만드는 중

---
+++ app/src/main/java/com/flagtag/wrinkle/activity/UserSelectActivity.java
@@ -0,0 +1,16 @@
+package com.flagtag.wrinkle.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.flagtag.wrinkle.R;
+
+public class UserSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_select);
+    }
+}
